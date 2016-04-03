b735b4ae4e66770d4da519ab9efa8a30fa63ac9a
Gavin Chan <freshgav@gmail.com>
2016-04-03 15:24:19 +0800

    implement verifyadmin() re:task1

--- rest-server-passport/routes/verify.js
+++ rest-server-passport/routes/verify.js
@@ -35,3 +35,39 @@ exports.verifyOrdinaryUser = function(req, res, next) {
     return next(err);
   }
 };
+
+exports.verifyAdmin = function(req, res, next) {
+  // check header, url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  console.log('Verifying token...');
+
+  // decode token
+  if (token) {
+    // verify secret, check if admin
+    console.log('Token provided, check if admin...');
+    jwt.verify(token, config.secretKey, function(err, decoded) {
+      if (err) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+      } else {
+        // token ok, check if admin
+        if (decoded._doc.admin) {
+          console.log('User has admin privileges.');
+          next();
+        } else {
+          var err = new Error('You do not have sufficient privileges.');
+          err.status = 401;
+          return next(err);
+        }
+      }
+    });
+  } else {
+    // no token, return an error
+    console.log('No token found.');
+    var err = new Error('No token provided!');
+    err.status = 403;
+    return next(err);
+  }
+};
+
